63dd91af6498ed6ac83f9cf0133fb58a87cea788
Dawid <corre14@gmail.com>
2025-07-26 23:20:32 +0200

    initial project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup functionality not implemented yet");
+};
+
+export const login = async (req, res) => {
+  res.send("login functionality not implemented yet");
+};
+
+export const logout = async (req, res) => {
+  res.send("Signup functionality not implemented yet");
+};
